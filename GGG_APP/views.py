686d510bb92730d5c1bdc65941496d7b5cf76556
Студент группы IS <is@sielom.local>
2025-12-11 13:40:24 +0500

    Added Filter

--- GGG_APP/views.py
+++ GGG_APP/views.py
@@ -1,21 +1,44 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Car
 
 def home(request):
-    """Главная страница со всеми автомобилями"""
+    """Главная страница со всеми автомобилями и поиском"""
     cars = Car.objects.all().order_by('-year')
-    return render(request, 'index.html', {'cars': cars})  
+    search_query = ''
+
+   
+    if request.method == 'GET' and 'q' in request.GET:
+        search_query = request.GET.get('q', '').strip()
+        if search_query:
+           
+            cars = Car.objects.filter(
+                Q(brand__icontains=search_query) |
+                Q(model__icontains=search_query) |
+                Q(color__icontains=search_query) |
+                Q(description__icontains=search_query)
+            ).order_by('-year')
+
+    
+    brands = Car.objects.values_list('brand', flat=True).distinct().order_by('brand')
+
+    return render(request, 'index.html', {
+        'cars': cars,
+        'search_query': search_query,
+        'brands': brands,
+    }) 
+
 
 def car_detail(request, car_id):
     """Страница деталей автомобиля"""
     car = get_object_or_404(Car, id=car_id)
 
-
+    
     similar_cars = Car.objects.filter(
         brand=car.brand
     ).exclude(id=car.id).order_by('-year')[:3]
 
-    return render(request, 'car_detail.html', {  
+    return render(request, 'car_detail.html', {
         'car': car,
         'similar_cars': similar_cars
     })
--- GGG_PROJECKT/urls.py
+++ GGG_PROJECKT/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from GGG_APP import views
+from GGG_APP import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,7 +26,6 @@ urlpatterns = [
     path('car/<int:car_id>/', views.car_detail, name='car_detail'),
 ]
 
-# Для обслуживания статических файлов во время разработки
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
